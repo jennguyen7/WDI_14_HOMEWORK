6fe4630ecb7fe90cff47917faff219afff52efc0
Jen <jen@192-168-1-5.tpgi.com.au>
2017-12-11 23:06:56 +1100

    Jen's Transport Hw

--- Jennifer/wk01/4-thu/transport.js
+++ Jennifer/wk01/4-thu/transport.js
@@ -47,7 +47,7 @@ Add additional lines
 */
 
 
-
+/*
 var alameinLineArr = ("Flinders Street, Richmond, East Richmond, Burnley, Hawthorn, Glenferrie").split(", ");
 //console.log(alameinLine);
 
@@ -56,6 +56,8 @@ var glenWaverleyLineArr = ("Flagstaff, Melbourne Central, Parliament, Richmond, 
 
 var sandgrihamLineArr = ("Southern Cross, Richmond, South Yarra, Prahran, Windsor").split(", ");
 //console.log(sandgrihamLine);
+
+*/
 
 /*
 //If I include the for loop it prints it out 4 times 
@@ -69,11 +71,11 @@ var journeyAl =  function(originCheck, destinationCheck) {
 }
 
 */
-
+/*
 
 var journeyAl =  function(originCheck, destinationCheck) {
 		var originCheckPrint = alameinLineArr.indexOf(originCheck);
-		var destinationCheckPrint = alameinLineArr.indexOf(destinationCheck) +1;
+		var destinationCheckPrint = alameinLineArr.indexOf(destinationCheck) + 1;
 		console.log(alameinLineArr.indexOf(destinationCheck) + " stops ; " +alameinLineArr.slice(originCheckPrint, destinationCheckPrint).toString());
 };
 
@@ -88,9 +90,9 @@ var journeySan =  function(originCheck, destinationCheck) {
 		var destinationCheckPrint = sandgrihamLineArr.indexOf(destinationCheck) +1;
 		console.log(sandgrihamLineArr.indexOf(destinationCheck) + " stops ; " +sandgrihamLineArr.slice(originCheckPrint, destinationCheckPrint).toString());
 };
-
-
+*/
 /*
+
 //Hard coding, doesn't allow you to get on at different lines
 
 var ptvJourney = function (originCheck, destinationCheck) {
@@ -108,13 +110,15 @@ var ptvJourney = function (originCheck, destinationCheck) {
 	return retVal;
 }
 
-*/
 
+*/
 
 // I'm trying to incorporate intersection and combinations
 //i.e. your destination is "Burnley" on the alameinLineArr but your origin is "Flagstaff" on glenLineArr
 //so you get in glenLineArr "Flagstaff", get off at "Richmond", get on alameinLineArr.
 //Did I just make my life more complicated by pushing elements into an array?
+
+/*
 
 var ptvJourney = function (originCheck, destinationCheck) {
 	var retVal= "";
@@ -129,8 +133,9 @@ var ptvJourney = function (originCheck, destinationCheck) {
 	return retval;
 }
 
+*/
 
-
+/*
 //found this block of code but not well understood how it is applied	
 Array.prototype.intersect = function intersect(a, ...b) {
 	    const c = function (a, b) {
@@ -144,6 +149,136 @@ Array.prototype.intersect = function intersect(a, ...b) {
 
  console.log(alameinLineArr.intersect(glenWaverleyLineArr, sandgrihamLineArr));
 // ["Richmond"];
+
+*/
+
+
+//2nd attempt
+/*
+var alameinLineArr = ("Flinders Street, Richmond, East Richmond, Burnley, Hawthorn, Glenferrie").split(", ");
+//console.log(alameinLineArr);
+
+var glenWaverleyLineArr = ("Flagstaff, Melbourne Central, Parliament, Richmond, Kooyong, Tooronga").split(", ");
+//console.log(glenWaverleyLineArr);
+
+var sandgrihamLineArr = ("Southern Cross, Richmond, South Yarra, Prahran, Windsor").split(", ");
+//console.log(sandgrihamLineArr);
+
+
+
+
+//trainlines.alamein[1]; output is "Richmond"
+//trainlines.alamein; //output is the entire array of alameinLineArr
+//this doesn't work when I use it in "FOR IN LOOPS!!! DOESNT RECOGNISE" I need to hard code it- see below in object of arrays!
+var trainLines = {
+	alameinLine: alameinLineArr,
+	glenWaverleyLine: glenWaverleyLineArr,
+	sandgrihamLine: sandgrihamLineArr 
+}
+*/
+
+var trainlines = {
+	alameinLine: ["Flinders Street", "Richmond", "East Richmond", "Burnley", "Hawthorn", "Glenferrie"],
+	glenWaverleyLine: ["Flagstaff", "Melbourne Central", "Parliament", "Richmond", "Kooyong", "Tooronga"],
+	sandgrihamLine: ["Southern Cross", "Richmond", "South Yarra", "Prahran", "Windsor"]
+}
+
+
+var originCheck = "";
+var destinationCheck = "";
+/*
+var journey = function (originCheck, destinationCheck) {
+	
+}
+*/
+
+var originCheck = "Flinders Street";
+var destinationCheck = "East Richmond";
+
+//for each key in the trainlines iterate through each train station 
+for (let key of Object.keys(trainlines)) {
+		var retVal = "undefined";
+		console.log(key, trainlines[key]);
+		
+		return = retVal;
+		/*if(originCheck === retVal.includes(originCheck) && destinationCheck === retVal.includes(destinationCheck)) {
+			console.log("Hello");
+		}else{
+			console.log("Please check your spelling!")
+		}	*/
+	}; 
+
+
+
+
+/*
+var fruits = {
+	fruit1: ["mango", "cherry", "apple"],
+	fruit2: ["banana", "grape", "kiwi"],
+	fruit3: ["aniseed", "nashi", "bean"]
+}
+
+
+Object.keys(fruits).forEach(function(key) {
+	console.log(key, fruits[key]);
+})
+
+
+
+for (let key of Object.keys(fruits)) {
+    console.log(key, fruits[key]);
+}
+
+*/
+
+/*
+var journey = function (originCheck, destinationCheck) {
+
+}
+*/
+
+
+
+
+//trainlines.alamein.includes("Richmond"); //output is TRUE
+
+
+
+//Object.keys(trainLines) //Output new array ["alameinLine", "glenWaverleyLines", "sandringhamLines"]
+/*
+var key = ""; //?cannot be a local variable inside the function. Why??
+
+
+for (let key in trainlines) {
+	console.log(trainlines[key]);
+	if (key.includes(originCheck) && key.includes(destinationCheck)) {
+		console.log("Hello");
+	}
+
+
+}
+*/
+
+
+/*
+
+//for in loops
+var fakeObject = {
+	a: ["too", "hot", "to", "touch"],
+	b: ["get", "lost", "hey"],
+	c: ["mine", "not", "yours"]
+}
+
+for (let key in fakeObject) {
+	//console.log(fakeObject[key]);
+	if ()
+}
+*/
+
+//for in VS for of loops
+//https://bitsofco.de/for-in-vs-for-of/
+
+
 
 
 
