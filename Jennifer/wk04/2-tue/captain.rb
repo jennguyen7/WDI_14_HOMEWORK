5589d69aefac7bdb288712c105643ed42be0e5d4
Jen <jen@Jens-MacBook-Pro.local>
2018-01-15 11:36:29 +1100

    hw

--- Jennifer/wk04/2-tue/captain.rb
+++ Jennifer/wk04/2-tue/captain.rb
@@ -62,7 +62,7 @@ require "pry"
 
 planeteers = ["Earth", "Wind", "Captain Planet", "Fire", "Water"]
 #Access the second value in planeteers.
-# planeteers[1]
+#planeteers[1]
 
 #Add "Heart" to the end of planeteers.
 #planeteersHeart = ["Earth", "Wind", "Captain Planet", "Fire", "Water"]
@@ -72,16 +72,44 @@ planeteers = ["Earth", "Wind", "Captain Planet", "Fire", "Water"]
 
 #Combine planeteers with rangers = ["Red", "Blue", "Pink", "Yellow", "Black"] and save the result in a variable called heroes.
 rangers = ["Red", "Blue", "Pink", "Yellow", "Black"]
-heroes = planeteers.concat(rangers)
+#heroes = planeteers.concat(rangers)
 
 #Alphabetize the contents of heroes using a method. The Ruby documentation might help.
-heroes.sort
+#heroes.sort
 
 #Randomize the contents of heroes using a method. The Ruby documentation might help.
-heroes.sample
+#heroes.shuffle!
+#if you put in heroes.shuffle then it returns a new array. heroes.shuffle! shuffles within the same array.
+
+#Select a random element from heroes using a method. The Ruby documentation might help.
+#heroes.sample
+
+#Select all elements in heroes that begin with "B" using a method. The Ruby documentation might help.
+#this finds the first instance
+#heroes.find { |hero| hero.include?("B") }
+
+#this finds all elements within the array that start with B into a NEW array
+#heroes.select { |hero| hero.include?("B") }
+
+#Initialize a hash called ninja_turtle with the properties name, weapon and radical. They should have values of "Michelangelo", "Nunchuks" and true respectively.
+#this is an object, not hash
+
+ninja_turtle = {
+	name: "Michelangelo",
+	weapon: "Nunchuks",
+	radical: true
+}
 
 
+#new hash
+ninja_turtle_hash = {"name" => "Michelangelo", "weapon" => "Nunchuks", "radical" => true}
 
+#Add a key age to ninja_turtle. Set it to whatever numerical value you'd like.
+#ninja_turtle_hash["age"] = 5
+
+#ninja_turtle[:age] = 5
+
+#Remove the radical key-value pair from ninja_turtle.
 
 
 
