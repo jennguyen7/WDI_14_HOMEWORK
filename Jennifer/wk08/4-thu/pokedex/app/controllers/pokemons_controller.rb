e1096b33c090d2e25c116f324fd9c439f6fd5e3c
Jen <jen@Jens-MacBook-Pro.local>
2018-02-02 11:45:32 +1100

    My data isn't saving in new and edit :.

---
+++ Jennifer/wk08/4-thu/pokedex/app/controllers/pokemons_controller.rb
@@ -0,0 +1,48 @@
+class PokemonsController < ApplicationController
+
+	def new
+	end
+
+	def index
+		@pokemons = Pokemon.all
+	end
+
+	def show
+		@pokemon = Pokemon.find(params[:id])
+	end
+
+	def create
+		pokemon = Pokemon.new
+		pokemon.species = params[:species]
+		pokemon.nickname = params[:nickname]
+		pokemon.level = params[:level]
+		pokemon.save
+		redirect_to '/'
+	end
+
+	def edit
+		@pokemon = Pokemon.find(params[:id])
+	end
+	
+	def update
+		@pokemon = Pokemon.find(params[:id])
+		@pokemon.species = params[:species]
+		@pokemon.nickname = params[:nickname]
+		@pokemon.save
+		redirect_to "/pokemons/#{params[:id]}"
+	end
+
+	def destroy
+		@pokemon = Pokemon.find(params[:id])
+		if @pokemon.destroy
+			redirect_to '/index'
+		else
+			render :show
+		end
+	end
+
+
+end
+
+
+
